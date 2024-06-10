8dad642f46fb25e7b5fd74a584be0a79ae335bf8
Sonu Thakur <sonu88ggn@gmail.com>
2024-06-10 16:40:05 +0530

    changes done

---
+++ Shooping App/UIserver/public/FINAL-PROJECT_Shopping_Application/logIn/login.js
@@ -0,0 +1,30 @@
+var myfucn = () => {
+    var credentials = {}
+    credentials.userName = document.querySelector("#eMail").value;
+    credentials.userPass = document.querySelector("#password").value;
+
+    
+   if(document.querySelector("#check").checked){
+    localStorage.setItem("userData",JSON.stringify(credentials));
+   } 
+   else {
+    localStorage.setItem("userData",null);
+
+   }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if(localStorage.getItem("userData")!= null){
+        // console.log("hello");
+        priFillData();
+        document.querySelector("#check").checked = true;
+       
+    }
+});
+
+var priFillData = () =>  {
+    var credentials = localStorage.getItem("userData");
+    credentials = JSON.parse(credentials);
+    document.querySelector("#eMail").value = credentials.userName;
+    document.querySelector("#password").value = credentials.userPass;
+}
