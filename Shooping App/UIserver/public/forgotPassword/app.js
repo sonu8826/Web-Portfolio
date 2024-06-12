efe50ba4492b8297d11be7a37989ffb924268d78
Sonu Thakur <sonu88ggn@gmail.com>
2024-06-12 17:01:03 +0530

    changes made in project

--- Shooping App/UIserver/public/forgotPassword/app.js
+++ Shooping App/UIserver/public/forgotPassword/app.js
@@ -7,21 +7,9 @@ var registerUser = () => {
     else {
         document.querySelector("#eRequire").style.display = "none";
     }
-    var pass = document.querySelector("#uPass").value;
-    var rpass = document.querySelector("#uPass2").value;
-        
-        var count = '';
-        var sCount = '';
-        for(i = 0; i<rpass.length; i++){
-            count += rpass[i];
-        }
-        for(j = 0; j<pass.length; j++){
-            sCount += pass[j];
-        }
-        if(count == sCount){
-            window.open("../logIn/singup.html", "_self")
-            return;
-        }else{
-            document.querySelector("#pwdErr").style.display = "block";
-        }
+   showmessage();
+}
+
+var showmessage = () => {
+    document.querySelector("#checkMail").style.display="block"
 }
